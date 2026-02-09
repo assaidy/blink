e0e3f56fa2a961bf6eff62bc4478933e8f0bb4e4
Ahmad Assaidy <someone@somewhere.something>
2026-02-09 21:18:31 +0200

    moved workers to a separate package

--- cmd/workers/main.go
+++ cmd/workers/main.go
@@ -2,119 +2,54 @@ package main
 
 import (
 	"context"
+	"log/slog"
 	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 	"time"
 
 	"github.com/assaidy/blink/app/db"
 	"github.com/assaidy/blink/app/repo"
+	"github.com/assaidy/workers"
 	"github.com/charmbracelet/log"
 )
 
-func main() {
-	workers := []Worker{
-		{
-			name:       "clean expired sessions",
-			job:        cleanExpiredSessionsJob,
-			tick:       24 * time.Hour,
-			timeout:    30 * time.Minute,
-			retries:    5,
-			retryDelay: 1 * time.Minute,
-		},
-		{
-			name:       "clean expired OTPs",
-			job:        cleanExpiredOtpsJob,
-			tick:       12 * time.Hour,
-			timeout:    30 * time.Minute,
-			retries:    5,
-			retryDelay: 1 * time.Minute,
-		},
-		{
-			name:       "clean deleted chat messages",
-			job:        cleanDeletedChatMessagesJob,
-			tick:       1 * time.Hour,
-			timeout:    30 * time.Minute,
-			retries:    5,
-			retryDelay: 1 * time.Minute,
-		},
-	}
-
-	quitCtx, quitCtxCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-
-	workersWg := sync.WaitGroup{}
-	defer workersWg.Wait()
-	for _, w := range workers {
-		workersWg.Go(func() {
-			w.start(quitCtx)
-		})
-	}
-
-	<-quitCtx.Done()
-	quitCtxCancel()
-	logger.Info("gracefully stopping workers. press Ctrl-c to force stop.")
-}
-
 var (
-	logger  = log.NewWithOptions(os.Stderr, log.Options{ReportTimestamp: true})
 	queries = repo.New(db.GetPool())
 )
 
-type Worker struct {
-	name       string
-	job        func(ctx context.Context) error
-	tick       time.Duration
-	timeout    time.Duration
-	retries    uint
-	retryDelay time.Duration
+func main() {
+	logger := slog.New(log.NewWithOptions(os.Stderr, log.Options{ReportTimestamp: true}))
+
+	workerManager := workers.NewWorkerManager(workers.WithLogger(logger))
+	workerManager.RegisterWorker(workers.NewWorker("clean expired sessions", cleanExpiredSessionsJob,
+		workers.WithTick(1*time.Hour),
+		workers.WithTimeout(10*time.Minute),
+		workers.WithRetries(2),
+		workers.WithRetryDelay(30*time.Second),
+	))
+	workerManager.RegisterWorker(workers.NewWorker("clean expired OTPs", cleanExpiredOtpsJob,
+		workers.WithTick(5*time.Minute),
+		workers.WithTimeout(30*time.Second),
+		workers.WithRetries(3),
+		workers.WithRetryDelay(10*time.Second),
+	))
+	workerManager.RegisterWorker(workers.NewWorker("clean deleted chat messages", cleanDeletedChatMessagesJob,
+		workers.WithTick(6*time.Hour),
+		workers.WithTimeout(15*time.Minute),
+		workers.WithRetries(2),
+		workers.WithRetryDelay(1*time.Minute),
+	))
+
+	workerManager.Start()
 }
 
-func (me Worker) start(workerCtx context.Context) {
-	logger.Info("worker started", "worker", me.name)
-	defer logger.Info("worker stopped", "worker", me.name)
-
-	ticker := time.NewTicker(me.tick)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			logger.Info("job started", "worker", me.name)
-
-			jobCtx, jobCtxCancel := context.WithTimeout(context.Background(), me.timeout)
-			if err := me.job(jobCtx); err != nil {
-				logger.Error("job failed", "worker", me.name, "error", err)
-
-				for i := uint(1); i <= me.retries; i++ {
-					<-time.After(me.retryDelay)
-
-					logger.Error("job retry started", "worker", me.name, "retry", i)
-					if err := me.job(jobCtx); err != nil {
-						logger.Error("job retry failed", "worker", me.name, "retry", i)
-						continue
-					}
-					logger.Error("job retry finished successfully", "worker", me.name, "retry", i)
-				}
-			} else {
-				logger.Info("job finished successfully", "worker", me.name)
-			}
-			jobCtxCancel()
-
-		case <-workerCtx.Done():
-			return
-		}
-	}
-}
-
-func cleanExpiredSessionsJob(ctx context.Context) error {
+func cleanExpiredSessionsJob(ctx context.Context, _ *slog.Logger) error {
 	return queries.BatchDeleteExpriredSessions(ctx)
 }
 
-func cleanExpiredOtpsJob(ctx context.Context) error {
+func cleanExpiredOtpsJob(ctx context.Context, _ *slog.Logger) error {
 	return queries.BatchDeleteExpiredOtps(ctx)
 }
 
-func cleanDeletedChatMessagesJob(ctx context.Context) error {
+func cleanDeletedChatMessagesJob(ctx context.Context, _ *slog.Logger) error {
 	return queries.BatchDeleteChatMessages(ctx)
 }
